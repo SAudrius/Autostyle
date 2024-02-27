51cc0aeceb04563e7b65f31481e0dd9dfb5f63e5
Edgaras <edasedgaras.dir@gmail.com>
2024-02-27 18:53:35 +0200

    feature: create footer for phone

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -6,7 +6,8 @@ import { montserrat, nunito } from '@/config/fonts';
 import { cn } from '@/config/utils';
 
 export const Footer = () => {
-  return <div className="bg-neutral-800 container pt-12 pb-48 text-white">
+  return  <div className="bg-neutral-800"> 
+  <div className="container pt-12 pb-48 text-white">
       <Image
         src="/assets/images/footer.png"
         alt="footer image"
@@ -17,7 +18,7 @@ export const Footer = () => {
          Discover how we can enhance your vehicles aesthetics and functionality.</p>
          <div className="flex flex-col gap-6 mt-8">
           <p className={cn("uppercase font-medium tracking-widest", montserrat.className)}>explore</p>
-          <div className={cn("flex flex-row gap-7 font-light", nunito.className)}>
+          <div className={cn("flex flex-row justify-between font-light", nunito.className)}>
             <Link className="w-40" href="#">Search</Link>
             <Link className="w-40" href="#">Brands</Link>
           </div>
@@ -25,23 +26,24 @@ export const Footer = () => {
          <div className="flex flex-col gap-6 mt-8">
           <p className={cn("uppercase font-medium tracking-widest", montserrat.className)}>resources</p>
           <div>
-          <div className={cn("flex flex-row gap-7 font-light", nunito.className)}>
+          <div className={cn("flex flex-row justify-between font-light", nunito.className)}>
             <Link className="w-40" href="#">Shipping Information</Link>
             <Link className="w-40" href="#">Returns & Exchanges</Link>
           </div>
-          <div className={cn("flex flex-row gap-7 font-light mt-4", nunito.className)}>
+          <div className={cn("flex flex-row justify-between font-light mt-4", nunito.className)}>
             <Link className="w-40" href="#">Privacy Policy</Link>
             <Link className="w-40" href="#">Terms of Use</Link>
           </div>
-          <div className={cn("flex flex-row gap-7 font-light mt-4", nunito.className)}>
+          <div className={cn("flex flex-row justify-between font-light mt-4", nunito.className)}>
             <Link className="w-40" href="#">Copyright Policy</Link>
             <Link className="w-40" href="#">Conditions of Sale</Link>
           </div>
-          <div className={cn("flex flex-row gap-7 font-light mt-4", nunito.className)}>
+          <div className={cn("flex flex-row justify-between font-light mt-4", nunito.className)}>
             <Link className="w-40" href="#">Need Help?</Link>
             <Link className="w-40" href="#">Contact Us</Link>
           </div>
           </div>
          </div>
+  </div>
   </div>
 };
