158b7dcf917a6124e862ae1e9c0cc4869357c30a
SAudrius <121971874+SAudrius@users.noreply.github.com>
2024-03-03 13:36:47 +0200

    fix(archt): change footer architecture

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { SectionExplore } from "./SectionExplore";
+import { SectionLogo } from "./SectionLogo";
+import { SectionResources } from "./SectionResources";
+
+export const Footer = () => {
+  return (
+    <div className="bg-neutral-800">
+      <div className="container pb-24 pt-12 text-white md:pt-20 lg:flex lg:flex-row lg:justify-between lg:gap-16">
+        <SectionLogo />
+        <div className="md:flex md:flex-row md:gap-8">
+          <SectionExplore />
+          <SectionResources />
+        </div>
+      </div>
+    </div>
+  );
+};
