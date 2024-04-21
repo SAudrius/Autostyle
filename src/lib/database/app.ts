23e9827ef2fc0eced084346042a305d4aa5f7e46
SAudrius <audrius@sabalys.com>
2024-04-21 22:09:47 +0300

    feat(dbQuery): function to send db sql

--- src/lib/database/app.ts
+++ src/lib/database/app.ts
@@ -1,14 +1,32 @@
-import "dotenv/config";
+"use server";
+import mysql from "mysql2/promise";
 
-import { connect } from "@planetscale/database";
+// import { dbConfigRemote } from "./config";
 
-export const config = {
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+type QueryRes<T> = [T, null] | [null, Error];
+
+export const dbQuery = async <T>(
+  sql: string,
+  valuesArr: (string | number | boolean)[] = [],
+): Promise<QueryRes<T>> => {
+  let conn;
+  try {
+    conn = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || "3306"),
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    });
+    // eslint-disable-next-line no-unused-vars
+    const [res, _fields] = await conn.execute(sql, valuesArr);
+    return [res as T, null];
+  } catch (err) {
+    return [null, err as Error];
+  } finally {
+    if (conn) conn.end();
+  }
 };
-
-export const conn = connect(config);
-// testDb
-// const showTables = await conn.execute("SHOW tables");
-// console.log("showTables ===", showTables);
