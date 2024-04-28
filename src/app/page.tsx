b949c05f5be3128141a3d1000f742a2c05445ded
SAudrius <audrius@sabalys.com>
2024-04-28 17:40:31 +0300

    fix: remove test function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
 "use client";
-// import { LoginButton } from "@/app/auth/_components/LoginButton";
+import { LoginButton } from "@/app/auth/_components/LoginButton";
 import { Button } from "@/components/ui/button";
-import { testDb } from "@/lib/data/test";
 
 export default function Home() {
-  const handleTest = () => {
-    testDb().then((res) => {
-      console.log(res);
-    });
-  };
   return (
     <div className="container flex h-screen items-center justify-center text-center text-2xl">
       <div>
         <h2 className="mb-4">Welcome to AutoStyle</h2>
-        {/* <LoginButton> */}
-        <Button variant="secondary" size="default" onClick={handleTest}>
-          To login
-        </Button>
-        {/* </LoginButton> */}
+        <LoginButton>
+          <Button variant="secondary" size="default">
+            To login
+          </Button>
+        </LoginButton>
       </div>
     </div>
   );
