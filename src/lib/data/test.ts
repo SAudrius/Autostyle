be3c325a62bcad563a303b1a71834e75788ff813
SAudrius <audrius@sabalys.com>
2024-04-28 16:18:49 +0300

    debug: x3

--- src/lib/data/test.ts
+++ src/lib/data/test.ts
@@ -2,6 +2,7 @@
 import { dbQuery } from "../database/app";
 
 export const testDb = async () => {
+  console.log("@TEST START");
   try {
     const sql = "SHOW TABLES";
     const [rows, error] = await dbQuery(sql);
@@ -9,9 +10,11 @@ export const testDb = async () => {
       throw new Error("somethink went wrong");
     }
     console.log(rows);
+    console.log("@TEST END @@ROW");
     return rows;
   } catch (error) {
     console.log("error ===", error);
+    console.log("@TEST END @ERR");
     return "somethink went wrong";
   }
 };
