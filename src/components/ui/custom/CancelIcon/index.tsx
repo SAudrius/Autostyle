075f09d5146c29e79d6b8cf584b2036977120b73
SAudrius <audrius@sabalys.com>
2024-05-10 20:48:54 +0300

    refactor: dark and light close icon

--- src/components/ui/custom/CancelIcon/index.tsx
+++ src/components/ui/custom/CancelIcon/index.tsx
@@ -3,11 +3,15 @@ import React from "react";
 
 interface CancelIconProps {
   onClick: () => void;
+  className?: string;
+  light?: boolean;
 }
+const DARK = "#161C1D";
+const LIGHT = "#ffffff";
 
-export const CancelIcon = ({ onClick }: CancelIconProps) => {
+export const CancelIcon = ({ onClick, className, light }: CancelIconProps) => {
   return (
-    <div onClick={onClick}>
+    <div onClick={onClick} className={className}>
       <svg
         width="18"
         height="18"
@@ -20,14 +24,14 @@ export const CancelIcon = ({ onClick }: CancelIconProps) => {
           height="23.9581"
           rx="0.748691"
           transform="matrix(0.707101 -0.707112 0.707101 0.707112 0 1.05859)"
-          fill="#161C1D"
+          fill={light ? DARK : LIGHT}
         />
         <rect
           width="1.49738"
           height="23.9581"
           rx="0.748691"
           transform="matrix(0.707101 0.707112 -0.707101 0.707112 16.9414 0)"
-          fill="#161C1D"
+          fill={light ? DARK : LIGHT}
         />
       </svg>
     </div>
