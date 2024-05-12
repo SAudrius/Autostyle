929786692dce4680ed60f44c0f065a7fe703a830
SAudrius <audrius@sabalys.com>
2024-05-12 19:10:04 +0300

    fix: opposite logic light dark

--- src/components/ui/custom/CancelIcon/index.tsx
+++ src/components/ui/custom/CancelIcon/index.tsx
@@ -5,13 +5,19 @@ interface CancelIconProps {
   onClick: () => void;
   className?: string;
   light?: boolean;
+  ariaLabel?: string;
 }
 const DARK = "#161C1D";
 const LIGHT = "#ffffff";
 
-export const CancelIcon = ({ onClick, className, light }: CancelIconProps) => {
+export const CancelIcon = ({
+  onClick,
+  className,
+  light,
+  ariaLabel,
+}: CancelIconProps) => {
   return (
-    <div onClick={onClick} className={className}>
+    <button onClick={onClick} className={className} aria-label={ariaLabel}>
       <svg
         width="18"
         height="18"
@@ -24,16 +30,16 @@ export const CancelIcon = ({ onClick, className, light }: CancelIconProps) => {
           height="23.9581"
           rx="0.748691"
           transform="matrix(0.707101 -0.707112 0.707101 0.707112 0 1.05859)"
-          fill={light ? DARK : LIGHT}
+          fill={light ? LIGHT : DARK}
         />
         <rect
           width="1.49738"
           height="23.9581"
           rx="0.748691"
           transform="matrix(0.707101 0.707112 -0.707101 0.707112 16.9414 0)"
-          fill={light ? DARK : LIGHT}
+          fill={light ? LIGHT : DARK}
         />
       </svg>
-    </div>
+    </button>
   );
 };
