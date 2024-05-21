c3a4c4b6e3457234dbfd7d67e69055226d068edb
SAudrius <audrius@sabalys.com>
2024-05-21 20:49:57 +0300

    fix: store provider

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -6,20 +6,16 @@ import menuSlice from "./slices/menuSlice";
 import modalSlice from "./slices/modalSlice";
 import searchSlice from "./slices/searchSlice";
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      auth: authSlice,
-      modal: modalSlice,
-      cart: cartSlice,
-      menu: menuSlice,
-      search: searchSlice,
-    },
-  });
-};
-
+export const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    modal: modalSlice,
+    cart: cartSlice,
+    menu: menuSlice,
+    search: searchSlice,
+  },
+});
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<typeof store.getState>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
+export type AppDispatch = typeof store.dispatch;
