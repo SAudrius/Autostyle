072a5166390665943b7bedb3eb1b945bd53b8eb9
SAudrius <audrius@sabalys.com>
2024-06-17 17:54:23 +0300

    feat: loading supsense for user details section & remove unused(commented) code

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -1,22 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import { Container } from "@/components/ui/custom";
 
 import { AccountInfo } from "./_components/AccountInfo";
 import { OrdersList } from "./_components/OrdersList";
+import { AccountInfoSkeleton } from "./_components/Ui/skeletons/AccountInfoSkeleton";
 
 const SettingsPage = async () => {
-  // eslint-disable-next-line no-unused-vars
-  // async function handleSingout() {
-  //   "use server";
-  //   console.log("TODO: handle singout");
-  // }
-
   return (
     <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-neutral-000 to-neutral-100 text-black">
       <Container className="grid pb-10 pt-10">
         <OrdersList />
-        <AccountInfo />
+        <Suspense fallback={<AccountInfoSkeleton />}>
+          <AccountInfo />
+        </Suspense>
       </Container>
     </div>
   );
