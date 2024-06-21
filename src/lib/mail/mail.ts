27f4cf29c66cae3867574201c067a008d3afb7e9
SAudrius <audrius@sabalys.com>
2024-06-21 08:57:47 +0300

    feat: created new function argument to select template

--- src/lib/mail/mail.ts
+++ src/lib/mail/mail.ts
@@ -1,35 +1,46 @@
 'use server'
 import { Resend } from "resend";
 
+import { ForgotPasswordTemplate } from "@/components/templates/ForgotPasswordTemplate";
 import { VerificationTemplate } from "@/components/templates/VerificationTemplate";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const sendMail = async (token: string, email: string) => {
+export const sendMail = async (token: string, email: string, template: "verification" | "forgotPassword" ) => {
   try {
-  console.log('send mail')
+  const selectedTemplate = () => {
+    if (template === 'verification') {
+      return {template: VerificationTemplate({verificationLink: `http://localhost:3000/auth/verification?token=${token}`}), title: 'Account verification'}
+    }
+    if (template === "forgotPassword") {
+      return {template: ForgotPasswordTemplate({resetPasswordLink: `http://localhost:3000/auth/reset-password?token=${token}`}), title: 'Change password'}
+    }
+  }
+  const currentTemplate = selectedTemplate() 
+
+  if (!currentTemplate) {
+    return {mailError:'Somethink went wrong'}
+  }
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data, error } = await resend.emails.send({
     from: 'AutoStyle Team Testing <autostyle@resend.dev>',
-    // to: [email]       TODO: email to send uncomment
+    // to: [email] TODO: email to send uncomment
     to:'audrius@sabalys.com',
-    subject: 'Account verification v5',
-    react: VerificationTemplate({verificationLink:`http://localhost:3000/auth/verification?token=${token}`})
+    subject: currentTemplate.title,
+    react: currentTemplate.template
   });
 
   if (error) {
-    console.log('error ===', error);
-    return { mailError: 'Something went wrong: ' + JSON.stringify(error) }; // Provide detailed error information
+    return { mailError: 'Something went wrong' };
   }
 
   if (error) {
-    return {mailError:'Somethink went wrong'}
+    return {mailError: 'Something went wrong' }
   }
 
-  return {mailSuccess: 'Mail send'}
+  return { mailSuccess: 'Mail send'}
 } catch (err){
-  console.log(err)
-  return {mailError:'Somethink went wrong'}}
+  return { mailError: 'Something went wrong'}}
 }
