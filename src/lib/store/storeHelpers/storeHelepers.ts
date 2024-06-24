f1c6ad3260cc997a81a7c65603d6f765d6a702d6
SAudrius <audrius@sabalys.com>
2024-06-24 15:59:02 +0300

    feat: storeHelpers for better usage of redux store and modal

---
+++ src/lib/store/storeHelpers/storeHelepers.ts
@@ -0,0 +1,39 @@
+'use client'
+
+import { Action } from '@reduxjs/toolkit';
+
+import { modalAnimateOff, modalAnimateOn, modalOff, modalOn } from "../slices/modalSlice";
+import { AppDispatch } from "../store";
+
+/**
+ * Turn on store modal with animation
+ * @param dispatch Redux dispatch
+ * @array dispathReducers Redux reducers 
+ */
+export const turnModalOn =  (dispatch: AppDispatch , dispathReducers?: Array<() => Action>) => {
+    if (dispathReducers) {
+        dispathReducers.forEach(func => dispatch(func()));
+    }
+    dispatch(modalOn());
+    dispatch(modalAnimateOn());
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+/**
+ * Turn off store modal with animation
+ * @param dispatch Redux dispatch
+ * @array dispathReducers Redux reducers before animation
+ * @array dispathReducersAfterAnimations Redux reducer after animations ends
+ */
+export const turnModalOff = (dispatch: AppDispatch, dispathReducers?: Array<() => Action>, dispathReducersAfterAnimations?: Array<() => Action>) => {
+    dispatch(modalAnimateOff());
+    if (dispathReducers) {
+        dispathReducers.forEach(func => dispatch(func()));
+    }
+    new Promise((resolve) => setTimeout(resolve, 300)).then(() => {
+      dispatch(modalOff());
+      if (dispathReducersAfterAnimations) {
+        dispathReducersAfterAnimations.forEach(func => dispatch(func()));
+      }
+    });
+}
