0e24e6996b8b4effe7653eb1a465c2405c1e99ab
SAudrius <audrius@sabalys.com>
2024-07-05 23:31:59 +0300

    feat: generate otp code helper

--- src/config/helpers.ts
+++ src/config/helpers.ts
@@ -1,5 +1,23 @@
 
 export const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo( { top: 0, behavior: "smooth" } );
 };
   
+export const generateRandomSixNumberCode = () => {
+    return parseInt( Math.floor( 100000 + Math.random() * 900000 ).toString() );
+}
+
+export const createExpiryTime = ( hours = 0, minutes = 0, seconds = 0 ) => {
+    const currentTime = new Date();
+    const expiryTime = new Date( currentTime.getTime() +
+      ( hours * 60 * 60 * 1000 ) +
+      ( minutes * 60 * 1000 ) +
+      ( seconds * 1000 )
+    );
+    return expiryTime.getTime();
+}
+
+export const convertToDbDate = ( timestamp:number ) => {
+    const date = new Date( timestamp );
+    return date.toISOString().slice( 0, 19 ).replace( 'T', ' ' );
+};
