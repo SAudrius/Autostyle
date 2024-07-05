3f6eb579353a921d8990b4bfd07f10c9cc8c14e6
SAudrius <audrius@sabalys.com>
2024-07-05 23:31:15 +0300

    feat: 3 send mail function for code or token or just mail

--- src/lib/mail/mail.ts
+++ src/lib/mail/mail.ts
@@ -1,46 +1,123 @@
 'use server'
+import { ChangeEmailTemplate } from "@components/templates/ChangeEmailTemplate";
+import { ChangetPasswordTemplate } from "@components/templates/ChangePasswordTemplate";
+import { ForgotPasswordTemplate } from "@components/templates/ForgotPasswordTemplate";
+import { VerificationTemplate } from "@components/templates/VerificationTemplate";
 import { Resend } from "resend";
 
-import { ForgotPasswordTemplate } from "@/components/templates/ForgotPasswordTemplate";
-import { VerificationTemplate } from "@/components/templates/VerificationTemplate";
+import { SuccessPasswordChangeTemplate } from "@/components/templates/SuccessPasswordChangeTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend( process.env.RESEND_API_KEY );
+
+export const sendMail = async ( email: string, template: 'successPassword' ) => {
+    try {
+        const selectedTemplate = () => {
+            if ( template === 'successPassword' ) {
+                return { template: SuccessPasswordChangeTemplate(), title: 'Account Verification' }
+            }
+        }
+        const currentTemplate = selectedTemplate() 
+
+        if ( !currentTemplate ) {
+            return { mailError:'Somethink went wrong' }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { data, error } = await resend.emails.send( {
+            from: 'AutoStyle Team Testing <autostyle@resend.dev>',
+            // to: [email] TODO: email to send uncomment
+            to:'audrius@sabalys.com',
+            subject: currentTemplate.title,
+            react: currentTemplate.template
+        } );
+
+        if ( error ) {
+            return { mailError: 'Something went wrong' };
+        }
+
+        if ( error ) {
+            return { mailError: 'Something went wrong' }
+        }
+
+        return { mailSuccess: 'Mail send' }
+    } catch ( err ){
+        return { mailError: 'Something went wrong' }
+    }
+}
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const sendMail = async (token: string, email: string, template: "verification" | "forgotPassword" ) => {
-  try {
-  const selectedTemplate = () => {
-    if (template === 'verification') {
-      return {template: VerificationTemplate({verificationLink: `http://localhost:3000/auth/verification?token=${token}`}), title: 'Account verification'}
+export const sendMailToken = async ( token: string, email: string, template: "verification" | "forgotPassword" | 'successPassword' ) => {
+    try {
+        const selectedTemplate = () => {
+            if ( template === 'verification' ) {
+                return { template: VerificationTemplate( { verificationLink: `http://localhost:3000/auth/verification?token=${token}` } ), title: 'Account Verification' }
+            }
+            if ( template === "forgotPassword" ) {
+                return { template: ForgotPasswordTemplate( { resetPasswordLink: `http://localhost:3000/auth/reset-password?token=${token}` } ), title: 'Change Password' }
+            }
+        }
+        const currentTemplate = selectedTemplate() 
+
+        if ( !currentTemplate ) {
+            return { mailError:'Somethink went wrong' }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { data, error } = await resend.emails.send( {
+            from: 'AutoStyle Team Testing <autostyle@resend.dev>',
+            // to: [email] TODO: email to send uncomment
+            to:'audrius@sabalys.com',
+            subject: currentTemplate.title,
+            react: currentTemplate.template
+        } );
+
+        if ( error ) {
+            return { mailError: 'Something went wrong' };
+        }
+
+        if ( error ) {
+            return { mailError: 'Something went wrong' }
+        }
+
+        return { mailSuccess: 'Mail send' }
+    } catch ( err ){
+        return { mailError: 'Something went wrong' }
     }
-    if (template === "forgotPassword") {
-      return {template: ForgotPasswordTemplate({resetPasswordLink: `http://localhost:3000/auth/reset-password?token=${token}`}), title: 'Change password'}
+}
+
+export const sendMailCode = async ( email: string, code:number, template: "changeEmail" | "changePassword" ) => {
+    try {
+        const selectedTemplate = () => {
+            if ( template === 'changeEmail' ) {
+                return { template: ChangeEmailTemplate( { code } ), title: 'Change email' }
+            }
+            if ( template === "changePassword" ) {
+                return { template: ChangetPasswordTemplate( { code } ), title: 'Change password' }
+            }
+        }
+        const currentTemplate = selectedTemplate() 
+  
+        if ( !currentTemplate ) {
+            return { mailError:'Somethink went wrong' }
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { data, error } = await resend.emails.send( {
+            from: 'AutoStyle Team Testing <autostyle@resend.dev>',
+            // to: [email] TODO: email to send uncomment
+            to:'audrius@sabalys.com',
+            subject: currentTemplate.title,
+            react: currentTemplate.template
+        } );
+  
+        if ( error ) {
+            return { mailError: 'Something went wrong' };
+        }
+  
+        if ( error ) {
+            return { mailError: 'Something went wrong' }
+        }
+  
+        return { mailSuccess: 'Mail send' }
+    } catch ( err ){
+        return { mailError: 'Something went wrong' }
     }
-  }
-  const currentTemplate = selectedTemplate() 
-
-  if (!currentTemplate) {
-    return {mailError:'Somethink went wrong'}
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { data, error } = await resend.emails.send({
-    from: 'AutoStyle Team Testing <autostyle@resend.dev>',
-    // to: [email] TODO: email to send uncomment
-    to:'audrius@sabalys.com',
-    subject: currentTemplate.title,
-    react: currentTemplate.template
-  });
-
-  if (error) {
-    return { mailError: 'Something went wrong' };
-  }
-
-  if (error) {
-    return {mailError: 'Something went wrong' }
-  }
-
-  return { mailSuccess: 'Mail send'}
-} catch (err){
-  return { mailError: 'Something went wrong'}}
 }
