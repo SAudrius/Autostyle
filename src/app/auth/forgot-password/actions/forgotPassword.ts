052c1c1aa3ffbd3b8ff56756f174c8422de075df
SAudrius <audrius@sabalys.com>
2024-07-08 23:54:50 +0300

    feat: action password validate

--- src/app/auth/forgot-password/actions/forgotPassword.ts
+++ src/app/auth/forgot-password/actions/forgotPassword.ts
@@ -2,32 +2,32 @@
 
 import { generateVerificationToken } from "@/lib/auth/tokens";
 import { getUserByEmail } from "@/lib/data/users";
-import { sendMail } from "@/lib/mail/mail";
+import { sendMailToken } from "@/lib/mail/mail";
 
-export const forgotPassword = async (email: string) => {
+export const forgotPassword = async ( email: string ) => {
 
-    if (!email) {
-        return {error: 'Something went wrong'}
+    if ( !email ) {
+        return { error: 'Something went wrong' }
     }
 
-    const existingUser = await getUserByEmail(email)
-    if (!existingUser?.email) {
-        return {error: 'Email does not exist'}
+    const existingUser = await getUserByEmail( email )
+    if ( !existingUser?.email ) {
+        return { error: 'Email does not exist' }
     }
-    if (!existingUser?.email_verified) {
-        return {error: 'Please verify email first'}
-    }
-
-    const newToken = await generateVerificationToken(existingUser.email, 'password');
-    if (!newToken) {
-      return {error: 'Something went wrong'}
+    if ( !existingUser?.email_verified ) {
+        return { error: 'Please verify email first' }
     }
 
-    const {mailError} = await sendMail(newToken, email, "forgotPassword")
-    if (mailError) {
-        return {error: 'Something went wrong'}
+    const newToken = await generateVerificationToken( existingUser.email, 'password' );
+    if ( !newToken ) {
+        return { error: 'Something went wrong' }
     }
 
-    return {success: 'Success! Check your email for instructions'}
+    const { mailError } = await sendMailToken( newToken, email, "forgotPassword" )
+    if ( mailError ) {
+        return { error: 'Something went wrong' }
+    }
+
+    return { success: 'Success! Check your email for instructions' }
 
 }
