acad270ac64d39528520bfd57fe1b3f0eb06dfad
SAudrius <audrius@sabalys.com>
2024-07-24 09:23:55 +0300

    fix: search active is visible on desktop

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -13,31 +13,31 @@ interface HeaderProps {
   transparent?: boolean;
 }
 
-export const Header = ({ transparent }: HeaderProps) => {
-  const modal = useAppSelector((state) => state.modal.value);
-  const search = useAppSelector((state) => state.search.active);
+export const Header = ( { transparent }: HeaderProps ) => {
+    const modal = useAppSelector( ( state ) => state.modal.value );
+    const search = useAppSelector( ( state ) => state.search.active );
 
-  return (
-    <header
-      className={cn(
-        "relative grid",
-        {
-          "bg-white": !transparent,
-        },
-        {
-          "overflow-hidden": !modal,
-        },
-        { "md:z-30": search },
-      )}
-    >
-      <Container className="flex items-center justify-between gap-4 bg-white py-5 md:gap-12 lg:gap-40">
-        <Logo />
-        <MobileMenu />
-      </Container>
-      <Container>
-        <DesktopMenu />
-      </Container>
-      <Line primary />
-    </header>
-  );
+    return (
+        <header
+            className={cn(
+                "relative grid",
+                {
+                    "bg-white": !transparent,
+                },
+                {
+                    "overflow-hidden": !modal,
+                },
+                { "md:z-40": search },
+            )}
+        >
+            <Container className="flex items-center justify-between gap-4 bg-white py-5 md:gap-12 lg:gap-40">
+                <Logo />
+                <MobileMenu />
+            </Container>
+            <Container>
+                <DesktopMenu />
+            </Container>
+            <Line primary />
+        </header>
+    );
 };
