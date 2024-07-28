cc311b9bebda01719e0fe915ac08aca9fcbe092d
SAudrius <audrius@sabalys.com>
2024-07-28 13:05:27 +0300

    feat: custom form elements for success, error, loading

--- src/components/ui/custom/FormSuccess/index.tsx
+++ src/components/ui/custom/FormSuccess/index.tsx
@@ -6,18 +6,30 @@ import { cn } from "@/config/utils";
 interface FormSuccessProps {
   message?: string;
   className?: string;
+  size?: 'small' | 'default';
 }
 
-export const FormSuccess = ({ message, className }: FormSuccessProps) => {
-  if (!message) return null;
-  return (
-    <div
-      className={cn(
-        `flex items-center justify-center gap-x-2 rounded-md bg-emerald-500/15 p-2 text-sm text-emerald-500 ${className}`,
-      )}
-    >
-      <CheckCircledIcon className="2-4 h-4" />
-      <p>{message}</p>
-    </div>
-  );
-};
+export const FormSuccess = ( { message, className, size = 'default' }: FormSuccessProps ) => {
+    if ( !message ) return null;
+
+    const selectClass = () => {
+        if ( size === 'default' ) {
+            return 'flex items-center justify-center gap-x-2 rounded-md bg-emerald-500/15 p-2 text-sm text-emerald-500'
+        } else {
+            return 'w-full rounded h-[36px] bg-emerald-300/50 px-4 py-[8px] text-base text-center text-emerald-500'
+        }
+    }
+    const divClass = selectClass()
+
+    return (
+        <div
+            className={cn(
+                `${divClass} flex justify-center items-center gap-[3px] ${className}`,
+            )}
+        >
+            <CheckCircledIcon className="h-4 mt-[2px]" />
+            <p className="h-full mb-[2px]">{message}</p>
+        </div>
+
+    );
+};                    
