c99d53e4b116045536ec7fe39079593928bb30d2
SAudrius <audrius@sabalys.com>
2024-07-28 11:53:27 +0300

    fix: delete userCode from DB after success

--- src/actions/validateOtp.ts
+++ src/actions/validateOtp.ts
@@ -1,16 +1,15 @@
 'use server'
 
 import { tokenDataByToken } from "@lib/auth/auth";
-import { getVerificationCodeById } from "@lib/data/verificationCodes";
+import { deleteVerificationCodesByUserId, getVerificationCodeByUserId } from "@lib/data/verificationCodes";
 import { otpCodeSchema } from "@lib/schemas"
 import { cookies } from "next/headers";
 
-export const validateOtp = async ( otpCode: { otpCode: string } ) => {
-    const keyword = 'password'
+export const validateOtp = async ( otpCode: { otpCode: string }, type: 'email' | 'password' ) => {
     const otpValid = otpCodeSchema.safeParse( otpCode );
     if ( !otpValid.success ) {
         console.log( 'otpValid ' );
-        return { error: "Values are not valid" };
+        return { error: "Must enter the code" };
     }
 
     const authCookie = cookies().get( 'auth' )
@@ -27,7 +26,7 @@ export const validateOtp = async ( otpCode: { otpCode: string } ) => {
 
     const userId = parseInt( jwtTokenData?.userId )
 
-    const verificationCodeData = await getVerificationCodeById( userId, keyword )
+    const verificationCodeData = await getVerificationCodeByUserId( userId, type )
 
     if ( !verificationCodeData ) {
         console.log( 'verificationCodeData ===', verificationCodeData );
@@ -36,9 +35,14 @@ export const validateOtp = async ( otpCode: { otpCode: string } ) => {
 
     if ( parseInt( verificationCodeData.code ) !== parseInt( otpCode.otpCode ) ) {
         console.log( ' verificationCodeData.code ===',  verificationCodeData.code );
-        return { error: 'Code is incorrect' }
+        return { error: 'Invalid Code' }
     }
 
-    return { error: 'Code confirmed' }
+    const deletedTokenRows = await deleteVerificationCodesByUserId( userId, type )
+    if ( deletedTokenRows?.affectedRows !== 1 ) {
+        return { error: 'Semething went wrong' }
+    }
+
+    return { success: 'Code Confirmed' }
 
 }
