1daeb5c3da47cb2e00111fb54913e6715d30b3df
SAudrius <audrius@sabalys.com>
2024-07-28 12:41:08 +0300

    fix(eslint): eslint sort

--- src/app/(protected)/account/_components/OrdersList/index.tsx
+++ src/app/(protected)/account/_components/OrdersList/index.tsx
@@ -6,22 +6,22 @@ import { nunito } from "@/config/fonts";
 import { cn } from "@/config/utils";
 
 export const OrdersList = () => {
-  return (
-    <div>
-      <h1
-        className={cn("text-2xl uppercase tracking-wide-20 drop-shadow-text")}
-      >
-        Settings
-      </h1>
-      <p className={cn("mt-5 text-base", nunito.className)}>
-        View your orders and manage your account
-      </p>
-      <SubLineHeading className="mt-5">
-        <h2 className="text-lg uppercase tracking-wide-20 drop-shadow-text">
-          Orders
-        </h2>
-      </SubLineHeading>
-      <EmptyOrders />
-    </div>
-  );
+    return (
+        <div>
+            <h1
+                className={cn( "text-2xl uppercase tracking-wide-20 drop-shadow-text" )}
+            >
+              Settings
+            </h1>
+            <p className={cn( "mt-5 text-base", nunito.className )}>
+              View your orders and manage your account
+            </p>
+            <SubLineHeading className="mt-5">
+                <h2 className="text-lg uppercase tracking-wide-20 drop-shadow-text">
+                  Orders
+                </h2>
+            </SubLineHeading>
+            <EmptyOrders />
+        </div>
+    );
 };
