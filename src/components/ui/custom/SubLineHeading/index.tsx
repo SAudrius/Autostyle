5d609c025b7b1446c090238f6fa1b95362a97e0b
SAudrius <audrius@sabalys.com>
2024-07-28 17:34:34 +0300

    fix(eslint)

--- src/components/ui/custom/SubLineHeading/index.tsx
+++ src/components/ui/custom/SubLineHeading/index.tsx
@@ -9,25 +9,25 @@ interface SubLineHeadingProps {
   logoutButton?: boolean;
 }
 
-export const SubLineHeading = ({
-  children,
-  className,
-  logoutButton = false,
-}: SubLineHeadingProps) => {
-  return (
-    <div
-      className={cn("grid ", className, {
-        "grid-cols-2": logoutButton,
-      })}
-    >
-      {!logoutButton && children}
-      {logoutButton && <div className="flex items-center">{children}</div>}
-      {logoutButton && (
-        <div className="flex items-center justify-end">
-          <LogoutButton />
+export const SubLineHeading = ( {
+    children,
+    className,
+    logoutButton = false,
+}: SubLineHeadingProps ) => {
+    return (
+        <div
+            className={cn( "grid ", className, {
+                "grid-cols-2": logoutButton,
+            } )}
+        >
+            {!logoutButton && children}
+            {logoutButton && <div className="flex items-center">{children}</div>}
+            {logoutButton && (
+                <div className="flex items-center justify-end">
+                    <LogoutButton />
+                </div>
+            )}
+            <Line className={cn( { "col-span-2": logoutButton } )}></Line>
         </div>
-      )}
-      <Line className={cn({ "col-span-2": logoutButton })}></Line>
-    </div>
-  );
+    );
 };
