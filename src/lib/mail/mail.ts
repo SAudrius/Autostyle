3a69dd1e89d8a5d7d7e336fec8087082a021b8c3
SAudrius <audrius@sabalys.com>
2024-07-28 17:35:56 +0300

    feat: mail sender (!wil be replaced to other lib!)

--- src/lib/mail/mail.ts
+++ src/lib/mail/mail.ts
@@ -2,14 +2,15 @@
 import { ChangeEmailTemplate } from "@components/templates/ChangeEmailTemplate";
 import { ChangetPasswordTemplate } from "@components/templates/ChangePasswordTemplate";
 import { ForgotPasswordTemplate } from "@components/templates/ForgotPasswordTemplate";
+import { SuccessEmailStepOneTemplate } from "@components/templates/SuccessEmailStepOneTemplate";
+import { SuccessEmailStepTwoTemplate } from "@components/templates/SuccessEmailStepTwoTemplate";
+import { SuccessPasswordChangeTemplate } from "@components/templates/SuccessPasswordChangeTemplate";
 import { VerificationTemplate } from "@components/templates/VerificationTemplate";
 import { Resend } from "resend";
 
-import { SuccessPasswordChangeTemplate } from "@/components/templates/SuccessPasswordChangeTemplate";
-
 const resend = new Resend( process.env.RESEND_API_KEY );
 
-export const sendMail = async ( email: string, template: 'successPassword' ) => {
+export const sendMail = async ( email: string, template: 'successPassword' | 'emailChanged' ) => {
     try {
         const selectedTemplate = () => {
             if ( template === 'successPassword' ) {
@@ -84,14 +85,20 @@ export const sendMailToken = async ( token: string, email: string, template: "ve
     }
 }
 
-export const sendMailCode = async ( email: string, code:number, template: "changeEmail" | "changePassword" ) => {
+export const sendMailCode = async ( email: string, code:number, template: "changeEmail" | "changePassword" | 'successEmailStepOne' | 'successEmailStepTwo'  ) => {
     try {
         const selectedTemplate = () => {
             if ( template === 'changeEmail' ) {
                 return { template: ChangeEmailTemplate( { code } ), title: 'Change email' }
             }
-            if ( template === "changePassword" ) {
+            else if ( template === "changePassword" ) {
                 return { template: ChangetPasswordTemplate( { code } ), title: 'Change password' }
+            }
+            else if ( template === 'successEmailStepOne' ) {
+                return { template: SuccessEmailStepOneTemplate( { code } ), title: 'Email Change' }
+            }
+            else if ( template === 'successEmailStepTwo' ) {
+                return { template: SuccessEmailStepTwoTemplate( { code } ), title: 'Email Confirmation' }
             }
         }
         const currentTemplate = selectedTemplate() 
