eea62f669aae73d98c013c4f96234a53bfcfeedc
SAudrius <audrius@sabalys.com>
2024-07-28 12:45:20 +0300

    fix(eslint): eslint sort

--- src/app/(protected)/account/_components/Ui/EmptyOrders/index.tsx
+++ src/app/(protected)/account/_components/Ui/EmptyOrders/index.tsx
@@ -1,18 +1,17 @@
+import { Button } from "@components/ui/button";
+import { nunito } from "@config/fonts";
+import { cn } from "@config/utils";
 import React from "react";
 
-import { Button } from "@/components/ui/button";
-import { nunito } from "@/config/fonts";
-import { cn } from "@/config/utils";
-
 export const EmptyOrders = () => {
-  return (
-    <div>
-      <h3 className={cn("mt-6 text-base", nunito.className)}>
-        You do not have any orders yet
-      </h3>
-      <Button className="mt-4" variant="secondary" size="lg">
-        Continue Shopping
-      </Button>
-    </div>
-  );
+    return (
+        <div className="">
+            <h3 className={cn( "mt-6 text-base", nunito.className )}>
+              You do not have any orders yet
+            </h3>
+            <Button className="mt-4 font-normal hover:font-medium" variant="secondary" size="lg">
+              Discover products
+            </Button>
+        </div>
+    );
 };
