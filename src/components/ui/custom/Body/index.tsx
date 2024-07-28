3c6f8f7dcabd403694e17530e7893879cd2a4a4a
SAudrius <audrius@sabalys.com>
2024-07-28 17:26:33 +0300

    feat!: popup for change user details, password, email ...

--- src/components/ui/custom/Body/index.tsx
+++ src/components/ui/custom/Body/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Footer, Header, Modal } from "@components/layout";
+import { Footer, Header, Modal, Popup } from "@components/layout";
 import { GlobalLoading } from "@components/layout/GlobalLoading";
 import React from "react";
 
@@ -12,26 +12,27 @@ interface BodyProps {
   children: React.ReactNode;
 }
 
-export const Body = ({ children }: BodyProps) => {
-  const modal = useAppSelector((state) => state.modal.value);
-  const globalLoading = useAppSelector((state) => state.globalLoading.loading);
-  return (
-    <body
-      className={cn(
-        `${montserrat.className}`,
-        {
-          "overflow-hidden": modal,
-        },
-        {
-          "overflow-hidden": globalLoading,
-        },
-      )}
-    >
-      <Modal />
-      {globalLoading && <GlobalLoading />}
-      <Header />
-      {children}
-      <Footer />
-    </body>
-  );
+export const Body = ( { children }: BodyProps ) => {
+    const modal = useAppSelector( ( state ) => state.modal.value );
+    const globalLoading = useAppSelector( ( state ) => state.globalLoading.loading );
+    return (
+        <body
+            className={cn(
+                `${montserrat.className}`,
+                {
+                    "overflow-hidden": modal,
+                },
+                {
+                    "overflow-hidden": globalLoading,
+                },
+            )}
+        >
+            <Modal />
+            <Popup />
+            {globalLoading && <GlobalLoading />}
+            <Header />
+            {children}
+            <Footer />
+        </body>
+    );
 };
