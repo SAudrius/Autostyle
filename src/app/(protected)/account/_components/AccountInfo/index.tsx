12156ac3484db022bdd38f0eaeea96e09efe2637
SAudrius <audrius@sabalys.com>
2024-07-28 12:03:53 +0300

    feat(css): desktop style for account page

--- src/app/(protected)/account/_components/AccountInfo/index.tsx
+++ src/app/(protected)/account/_components/AccountInfo/index.tsx
@@ -7,60 +7,64 @@ import { SubLineHeading } from "@/components/ui/custom/SubLineHeading";
 import { userInfo } from "./actions/userInfo";
 
 export const AccountInfo = async () => {
-  const userData = await userInfo();
-  if (!userData) {
+    const userData = await userInfo();
+    if ( !userData ) {
     // TODO: logout or display message
-    return;
-  }
-  return (
-    <>
-      <SubLineHeading logoutButton className="mt-8">
-        <h2 className="text-lg uppercase tracking-wide-20 drop-shadow-text">
-          Account
-        </h2>
-      </SubLineHeading>
-      <UserInfoField
-        className="mt-6"
-        field="Name"
-        value={userData.firstName || "Not provided"}
-      />
-      <UserInfoField
-        className="mt-3"
-        field="Last name"
-        value={userData.lastName || "Not provided"}
-      />
-      <UserInfoField
-        className="mt-3"
-        field="country"
-        value={userData.country || "Not provided"}
-      />
-      <UserInfoField
-        className="mt-3"
-        field="city"
-        value={userData.city || "Not provided"}
-      />
-      <UserInfoField
-        className="mt-3"
-        field="Address"
-        value={userData.address || "Not provided"}
-        infoValues={"values"}
-      />
-      <Line className="mb-4 mt-4" />
-      <UserInfoField
-        className="mt-3"
-        field="Email"
-        value={userData.email || "Not provided"}
-        linkLabel="Change email"
-        linkHref="/auth/change-email"
-      />
-      <Line className="mb-4 mt-4" />
-      <UserInfoField
-        className="mt-3"
-        field="Password"
-        value="********"
-        linkLabel="Change password"
-        linkHref="/auth/change-password"
-      />
-    </>
-  );
+        return;
+    }
+
+    return (
+        <div className="lg:mt-[60px]">
+            <SubLineHeading logoutButton className="mt-8">
+                <h2 className="text-lg uppercase tracking-wide-20 drop-shadow-text">
+                    Account
+                </h2>
+            </SubLineHeading>
+            <UserInfoField
+                className="mt-6"
+                field="Name"
+                value={userData.data?.first_name || "Not provided"}
+            />
+            <UserInfoField
+                className="mt-3"
+                field="Last name"
+                value={userData.data?.last_name || "Not provided"}
+            />
+            <UserInfoField
+                className="mt-3"
+                field="country"
+                value={userData.data?.country || "Not provided"}
+            />
+            <UserInfoField
+                className="mt-3"
+                field="city"
+                value={userData.data?.city || "Not provided"}
+            />
+            <UserInfoField
+                className="mt-3"
+                field="Address"
+                value={userData.data?.address || "Not provided"}
+                userData={userData}
+                buttonLabel="Change details"
+                haveButton
+            />
+            <Line className="mb-4 mt-4" />
+            <UserInfoField
+                className="mt-3"
+                field="Email"
+                value={userData.data?.email || "Not provided"}
+                buttonLabel="Change email"
+                haveButton
+            />
+            <Line className="mb-4 mt-4" />
+            <UserInfoField
+                className="mt-3"
+                field="Password"
+                value="********"
+                userData={userData}
+                buttonLabel="Change password"
+                haveButton
+            />
+        </div>
+    );
 };
--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -7,16 +7,16 @@ import { OrdersList } from "./_components/OrdersList";
 import { AccountInfoSkeleton } from "./_components/Ui/skeletons/AccountInfoSkeleton";
 
 const SettingsPage = async () => {
-  return (
-    <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-neutral-000 to-neutral-100 text-black">
-      <Container className="grid pb-10 pt-10">
-        <OrdersList />
-        <Suspense fallback={<AccountInfoSkeleton />}>
-          <AccountInfo />
-        </Suspense>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-neutral-000 to-neutral-100 text-black">
+            <Container className="grid pb-10 pt-10 lg:grid-cols-[1fr,365px] lg:gap-6">
+                <OrdersList />
+                <Suspense fallback={<AccountInfoSkeleton />}>
+                    <AccountInfo />
+                </Suspense>
+            </Container>
+        </div>
+    );
 };
 
 export default SettingsPage;
