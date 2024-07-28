a2676810589f4f9bd88c948f0a59bf6350458601
SAudrius <audrius@sabalys.com>
2024-07-28 17:31:53 +0300

    feat: error popup for main erros

---
+++ src/components/popups/ErrorPopup/index.tsx
@@ -0,0 +1,16 @@
+import { nunito } from '@config/fonts'
+import { cn } from '@config/utils'
+import React from 'react'
+
+export const ErrorPopup = () => {
+    return (
+        <div className={cn( "inline-block w-[400px] rounded bg-neutral-100 p-8" )}>
+            <h3 className="nunit text-center text-xl tracking-wide-12">
+                Procced password change
+            </h3>
+            <p className={cn( "mt-4 text-center", nunito.className )}>
+              An unexpected error has occurred. Please try again later.
+            </p>
+        </div>
+    )
+}
