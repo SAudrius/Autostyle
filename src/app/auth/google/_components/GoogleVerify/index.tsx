8b3340f317687203060cb4e4e729dba4ffb9666f
SAudrius <audrius@sabalys.com>
2024-08-05 15:45:33 +0300

    fix(eslint): code spacing

--- src/app/auth/google/_components/GoogleVerify/index.tsx
+++ src/app/auth/google/_components/GoogleVerify/index.tsx
@@ -10,45 +10,45 @@ import { useAppDispatch } from "@/lib/hooks";
 import { storeLogin } from "@/lib/store/slices/authSlice";
 
 export const GoogleVerify = () => {
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const [success, setSuccess] = useState<undefined | string>("");
-  const [error, setError] = useState<undefined | string>("");
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
-  const onSubmit = useCallback(() => {
-    if (!code) {
-      setError("Mising token");
-      return null;
-    }
-    const googleResponse = async () => {
-      try {
-        const fetchGoogleResponse = await fetchGoogleCode(code);
+    const router = useRouter();
+    const dispatch = useAppDispatch();
+    const [ success, setSuccess ] = useState<undefined | string>( "" );
+    const [ error, setError ] = useState<undefined | string>( "" );
+    const searchParams = useSearchParams();
+    const code = searchParams.get( "code" );
+    const onSubmit = useCallback( () => {
+        if ( !code ) {
+            setError( "Mising token" );
+            return null;
+        }
+        const googleResponse = async () => {
+            try {
+                const fetchGoogleResponse = await fetchGoogleCode( code );
 
-        setSuccess(fetchGoogleResponse?.success);
-        setError(fetchGoogleResponse?.error);
-      } catch {
-        setError("somethink went wrong");
-      } finally {
-        dispatch(storeLogin());
-        router.push("/account");
-      }
-    };
-    googleResponse();
+                setSuccess( fetchGoogleResponse?.success );
+                setError( fetchGoogleResponse?.error );
+            } catch {
+                setError( "somethink went wrong" );
+            } finally {
+                dispatch( storeLogin() );
+                router.push( "/account" );
+            }
+        };
+        googleResponse();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [code]);
+    }, [ code ] );
 
-  useEffect(() => {
-    onSubmit();
-  }, [onSubmit]);
+    useEffect( () => {
+        onSubmit();
+    }, [ onSubmit ] );
 
-  return (
-    <div className="gird col-span-1">
-      {!success && !error && (
-        <PulseLoader className="text-center" color="#000" />
-      )}
-      <FormSuccess message={success} />
-      {!success && <FormError message={error} />}
-    </div>
-  );
+    return (
+        <div className="gird col-span-1">
+            {!success && !error && (
+                <PulseLoader className="text-center" color="#000" />
+            )}
+            <FormSuccess message={success} />
+            {!success && <FormError message={error} />}
+        </div>
+    );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import { LoginButton } from "@/app/auth/_components/LoginButton";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="bg-white">
-      <div className="container flex h-screen items-center justify-center text-center text-2xl">
-        <div>
-          <h2 className="mb-4">Welcome to AutoStyle</h2>
-          <LoginButton>
-            <Button variant="secondary" size="default">
-              To login
-            </Button>
-          </LoginButton>
+    return (
+        <div className="bg-white">
+            <div className="container flex h-screen items-center justify-center text-center text-2xl">
+                <div>
+                    <h2 className="mb-4">Welcome to AutoStyle</h2>
+                    <LoginButton>
+                        <Button variant="secondary" size="default">
+                          To login
+                        </Button>
+                    </LoginButton>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
