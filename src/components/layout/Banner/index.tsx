2428c3bd6260ec2ef861787461ad2d4ccb5532cd
SAudrius <audrius@sabalys.com>
2024-08-07 11:50:06 +0300

    feat: banner hidden for future

--- src/components/layout/Banner/index.tsx
+++ src/components/layout/Banner/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Banner = () => {
     return (
-        <div className='w-full h-10 bg-black flex justify-center items-center text-sm font-thin'><p className=' text-white '>⚠️ This is a non-production page and may contain potential bugs. ⚠️</p></div>
+        <div className='hidden w-full h-9 bg-black justify-center items-center text-sm font-thin'><p className=' text-white '>This page is currently under development and may have some issues.</p></div>
     )
 }
