01e5cfc1ea60f9049025aa6d3499cf6a382dfbdf
SAudrius <audrius@sabalys.com>
2024-08-14 16:31:30 +0300

    feat(mobile): show only 4 cards on mobile with button "show more" for better ux

--- src/app/_components/DiscoverySection/index.tsx
+++ src/app/_components/DiscoverySection/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 
+import { Button } from '@/components'
 import { BackgroundIcon } from '@/components/ui/custom/BackgroundIcon'
 import { cn, montserrat } from '@/config'
 
@@ -10,19 +12,33 @@ interface DiscoverySectionProps {
 }
 
 export const DiscoverySection = ( { className }: DiscoverySectionProps ) => {
+    const [ displayMore, setDisplayMore ] = useState( false )
+
     return (
-        <section className={cn( 'pt-20 pb-20 md:pt-[120px] md:pb-[180px] bg-discovery-gradient relative overflow-hidden', className )}>
+        <section className={cn( 'bg-discovery-gradient relative overflow-hidden pt-20 pb-12 md:pt-[120px] md:pb-[120px] lg:pb-[180px]', className )}>
             <h2 className={cn( 'relative container text-neutral-800 font-medium uppercase text-[20px] lg:text-[24px] tracking-wide-20 text-center z-10', montserrat.className )} >Discover Leading<br className='sm:hidden'/> Auto Brands</h2>
-            <div className='relative container gap-4 sm:gap-6 md:gap-8 grid m-auto lg:grid-cols-12 mt-12 md:mt-20 md:grid-cols-2 z-10'>
-                <DiscoveryCard className='lg:col-span-4 md:justify-start' brand='Mercedes' description='Elegant Performance' imgClass="bg-[url('https://Autostyle.b-cdn.net/mercedes-card.png')]"/>
-                <DiscoveryCard className='lg:col-span-4 md:justify-end' brand='BMW' description='Driving Pleasure' imgClass="bg-[url('https://Autostyle.b-cdn.net/bmw-card.png')]"/>
-                <DiscoveryCard className='lg:col-span-4 md:justify-end' brand='Volkswagen' description='Precision Engineering' imgClass="bg-[url('https://Autostyle.b-cdn.net/volkswagen-card.png')]"/>
-                <DiscoveryCard className='lg:col-span-4 md:justify-start' brand='AUDI' description='Art of Progress' imgClass="bg-[url('https://Autostyle.b-cdn.net/audi-card.png')]" />
-                <DiscoveryCard className='lg:col-span-4 md:justify-start' brand='Volvo' description='Unmatched Safety' imgClass="bg-[url('https://Autostyle.b-cdn.net/volvo-card.png')]" />
-                <DiscoveryCard className='lg:col-span-4 md:justify-end' brand='Toyota' description='Trusted Performance' imgClass="bg-[url('https://Autostyle.b-cdn.net/toyota-card.png')]"/>
-                <DiscoveryCard className='lg:col-span-4 lg:col-start-3 md:justify-end' brand='Nissan' description='Dynamic Innovation' imgClass="bg-[url('https://Autostyle.b-cdn.net/nissan-card.png')]" />
-                <DiscoveryCard className='lg:col-span-4 lg:col-start-7 md:justify-start' brand='Honda' description='Engineering Excellence' imgClass="bg-[url('https://Autostyle.b-cdn.net/honda-card.png')]" />
+            <div className='relative z-10 container mt-12 grid m-auto gap-4 sm:gap-6 md:gap-8 md:mt-20 md:grid-cols-2 lg:grid-cols-12'>
+                <DiscoveryCard className='lg:col-span-4' brand='Mercedes' description='Elegant Performance' imgClass="bg-[url('https://Autostyle.b-cdn.net/mercedes-card.png')]"/>
+                <DiscoveryCard className='lg:col-span-4' brand='AUDI' description='Art of Progress' imgClass="bg-[url('https://Autostyle.b-cdn.net/audi-card.png')]" />
+                <DiscoveryCard className='lg:col-span-4' brand='Volkswagen' description='Precision Engineering' imgClass="bg-[url('https://Autostyle.b-cdn.net/volkswagen-card.png')]"/>
+                <DiscoveryCard className='lg:col-span-4' brand='BMW' description='Driving Pleasure' imgClass="bg-[url('https://Autostyle.b-cdn.net/bmw-card.png')]"/>
+                <DiscoveryCard className={cn( 'hidden md:flex lg:col-span-4' )} brand='Volvo' description='Unmatched Safety' imgClass="bg-[url('https://Autostyle.b-cdn.net/volvo-card.png')]" />
+                <DiscoveryCard className={cn( 'hidden md:flex lg:col-span-4' )} brand='Toyota' description='Trusted Performance' imgClass="bg-[url('https://Autostyle.b-cdn.net/toyota-card.png')]"/>
+                <DiscoveryCard className={cn( 'hidden md:flex lg:col-span-4 lg:col-start-3' )} brand='Nissan' description='Dynamic Innovation' imgClass="bg-[url('https://Autostyle.b-cdn.net/nissan-card.png')]" />
+                <DiscoveryCard className={cn( 'hidden md:flex lg:col-span-4 lg:col-start-7' )} brand='Honda' description='Engineering Excellence' imgClass="bg-[url('https://Autostyle.b-cdn.net/honda-card.png')]" />
+                <div className={cn( 'relative transition-height-visibility-css overflow-hidden h-0 md:hidden', 
+                { "h-[2073px] overflow-hidden" : displayMore },
+                { "h-0 visible" : !displayMore },  
+                )}>
+                    <div className='h-auto grid gap-4 sm:gap-6 m-auto '>
+                        <DiscoveryCard className="flex" brand='Volvo' description='Unmatched Safety' imgClass="bg-[url('https://Autostyle.b-cdn.net/volvo-card.png')]" />
+                        <DiscoveryCard className="flex" brand='Toyota' description='Trusted Performance' imgClass="bg-[url('https://Autostyle.b-cdn.net/toyota-card.png')]"/>
+                        <DiscoveryCard className="flex" brand='Nissan' description='Dynamic Innovation' imgClass="bg-[url('https://Autostyle.b-cdn.net/nissan-card.png')]" />
+                        <DiscoveryCard className="flex" brand='Honda' description='Engineering Excellence' imgClass="bg-[url('https://Autostyle.b-cdn.net/honda-card.png')]" />
+                    </div>
+                </div>
             </div>
+            <Button className='m-auto md:hidden block mt-6 relative z-10' variant='underline' onClick={() => setDisplayMore(prevState => !prevState)}>{displayMore ? "Show Less" : "Show More" }</Button>
             <BackgroundIcon className='absolute top-[-660px] rotate-[150deg] left-[-495px]'/>
             <BackgroundIcon className='absolute bottom-[-110px] rotate-[-22deg] right-[-430px]'/>
         </section>
